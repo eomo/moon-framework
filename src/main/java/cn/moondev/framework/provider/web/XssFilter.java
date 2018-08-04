8c57fc18ff8ac4ebb14bd6abd19509d80e98f5a8
skye <skye@lwork.com>
2018-08-04 15:57:52 +0800

    ADDED: ServeletRequest XSS包装类

---
+++ src/main/java/cn/moondev/framework/provider/web/XssFilter.java
@@ -0,0 +1,32 @@
+package cn.moondev.framework.provider.web;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+
+public class XssFilter implements Filter {
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        if (servletRequest instanceof HttpServletRequest) {
+            HttpServletRequest request = (HttpServletRequest) servletRequest;
+            XssHttpServletRequestWrapper requestWrapper = new XssHttpServletRequestWrapper(request);
+            filterChain.doFilter(requestWrapper, servletResponse);
+        }
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
